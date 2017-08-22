d34ae81bbc5348ff8edde8e9e30cb6d370df8b8d
w6ye <w6ye@ucsd.edu>
2017-08-22 08:41:29 -0700

    Descriptors, modify DistinctSiteProperty to get features regarding to CN, and extended to mixed species

--- veidt/descriptors.py
+++ veidt/descriptors.py
@@ -8,6 +8,10 @@ from __future__ import division, print_function, unicode_literals, \
 import numpy as np
 import pandas as pd
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+from pymatgen.core.periodic_table import get_el_sp
+
+from pymatgen.analysis.chemenv.coordination_environments.coordination_geometry_finder import LocalGeometryFinder
+from pymatgen.analysis.chemenv.coordination_environments.chemenv_strategies import SimplestChemenvStrategy
 
 from veidt.abstract import Describer
 
@@ -88,31 +92,101 @@ class FuncGenerator(Describer):
 
 class DistinctSiteProperty(Describer):
     """
-    Constructs a descriptor based on properties of distinct sites in a
-    structure. For now, this assumes that there is only one type of species in
-    a particular Wyckoff site.
+    Constructs a descriptor based on properties of distinct sites with different coordination number in a
+    structure.
     """
     #todo: generalize to multiple sites with the same Wyckoff.
 
-    def __init__(self, wyckoffs, properties, symprec=0.1):
+    def __init__(self,properties,CNs=None, symprec=0.1):
         """
-        :param wyckoffs: List of wyckoff symbols. E.g., ["48a", "24c"]
-        :param properties: Sequence of specie properties. E.g., ["atomic_radius"]
-        :param symprec: Symmetry precision for spacegroup determination.
+        :param CNs: List of coordination numbers of distintic sites. E.g., [8, 6, 4], return results for full
+        possible CNs in the structure if CNs == None :param properties: Sequence of specie properties. E.g.,
+        ["atomic_radius"], if "ionic_radius" is in the list, the structure should  be oxideation_state decorated
+        otherwise attribute error raised. :param symprec: Symmetry precision for spacegroup determination.
         """
-        self.wyckoffs = wyckoffs
+        self.CNs = CNs
         self.properties = properties
         self.symprec = symprec
 
-    def describe(self, structure):
+    def describe(self, structure,exclude_ele=['O']):
+        """
+
+        :param structure: Pymatgen Structure Object, if ionic radius is in the property list, structure should be os
+                            decorated
+        :param exclude_ele: list of elements not to be considered, default ['O']
+        :return: DataFrame with properties averaged for each cn.
+        """
         a = SpacegroupAnalyzer(structure, self.symprec)
-        symm = a.get_symmetrized_structure()
+        #symm = a.get_symmetrized_structure()
         data = []
         names = []
-        for w in self.wyckoffs:
-            site = symm.equivalent_sites[symm.wyckoff_symbols.index(w)][0]
+        cn_sites = self.get_cn_sites(structure=structure,
+                                     exclude_ele=exclude_ele,
+                                     maximum_distance_factor=1.5)
+        for cn in self.CNs if self.CNs else cn_sites:
+            species= [i.specie for i in cn_sites[cn]]
+            spe_occu = {spe:species.count(spe) for spe in set(species)}
             for p in self.properties:
-                data.append(getattr(site.specie, p))
-                names.append("%s-%s" % (w, p))
+                if p == 'X':
+                    avg_p = self.get_averaged_X(spe_occu)
+                else:
+                    avg_p = np.average([getattr(spe,p) for spe,occ in spe_occu.items()],
+                                       weights=[occ for spe,occ in spe_occu.items()])
+                data.append(avg_p)
+                names.append("%s-%s" % (cn, p))
         return pd.Series(data, index=names)
+
+
+    def get_cn_sites(self,structure,exclude_ele=['O'],maximum_distance_factor=1.5):
+        """
+
+        :param structure: Pymatgen structure Object
+        :param exclude_ele: list of elements not to be considered, eg ['O']
+        :param maximum_distance_factor:
+        :return: a dictionary in the format {cn_1:[sites with coordination number of cn_1]}
+        """
+        lgf = LocalGeometryFinder()
+        lgf.setup_parameters(structure_refinement='none')
+        lgf.setup_structure(structure)
+        se = lgf.compute_structure_environments(maximum_distance_factor=maximum_distance_factor)
+        default_strategy = SimplestChemenvStrategy(se)
+        cn_sites = {}
+        for eqslist in se.equivalent_sites:
+            eqslist = [i for i in eqslist if i.specie.symbol not in exclude_ele]
+            if not eqslist:
+                continue
+            site = eqslist[0]
+            ces = default_strategy.get_site_coordination_environments(site)
+            ce = ces[0]
+            cn = int(ce[0].split(':')[1])
+            if cn in cn_sites:
+                cn_sites[cn].extend(eqslist)
+
+            else:
+                cn_sites.update({cn:[site for site in eqslist]})
+
+        return cn_sites
+
+    def get_averaged_X(self,spe_occu):
+        """
+        Calcualte averaged electronnegtivity of two mixed species
+        :param spe_occu: specie in string or dict in the format {el1:amt1,el2:amt2}
+        :return:return the mean of electronegtivity from definition (Binding energy)
+                cf https://www.wikiwand.com/en/Electronegativity
+        """
+        # make sure spe does not contain charge
+        o = get_el_sp('O2-')
+        if len(spe_occu) < 2:
+            el = get_el_sp(list(spe_occu.keys())[0])
+            return el.X
+        else:
+            avg_eneg = 0
+            factor = sum([v for k, v in spe_occu.items()])
+            for s, amt in spe_occu.items():
+                el = get_el_sp(s)
+                avg_eneg += (amt/factor) * (el.X - o.X) ** 2
+
+            return np.abs(o.X - (np.sqrt(avg_eneg)))
+
+
 
--- veidt/tests/test_descriptors.py
+++ veidt/tests/test_descriptors.py
@@ -41,48 +41,100 @@ class GeneratorTest(unittest.TestCase):
         self.assert_(True)
 
 
+#class DistinctSitePropertyTest(unittest.TestCase):
+
+#    def setUp(self):
+#        self.li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+#                                                     "Li2O.cif"))
+#        self.na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+#                                                     "Na2O.cif"))
+#        self.describer = DistinctSiteProperty(['8c', '4a'],
+#                                              ["Z", "atomic_radius"])
+
+
+#    def test_describe(self):
+#        descriptor = self.describer.describe(self.li2o)
+#        self.assertAlmostEqual(descriptor["8c-Z"], 3)
+#        self.assertAlmostEqual(descriptor["8c-atomic_radius"], 1.45)
+#        descriptor = self.describer.describe(self.na2o)
+#        self.assertEqual(descriptor["4a-Z"], 8)
+#        self.assertEqual(descriptor["4a-atomic_radius"], 0.6)
+
+#    def test_describe_all(self):
+#        df = pd.DataFrame(self.describer.describe_all([self.li2o, self.na2o]))
+#        self.assertEqual(df.iloc[0]["8c-Z"], 3)
+#        self.assertEqual(df.iloc[0]["8c-atomic_radius"], 1.45)
+
+
 class DistinctSitePropertyTest(unittest.TestCase):
 
     def setUp(self):
-        self.li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                     "Li2O.cif"))
-        self.na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                     "Na2O.cif"))
-        self.describer = DistinctSiteProperty(['8c', '4a'],
-                                              ["Z", "atomic_radius"])
+        self.y3al5o12 = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                         "Y3Al5O12.cif"))
+        self.basg = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                     "Ba3Al2Ge2SiO12.cif"))
+        self.y3al5o12.add_oxidation_state_by_element({"Y": +3, "Al": +3, "O": -2})
+        self.basg.add_oxidation_state_by_element({"Ba": +2, "Al": +3, "Si": +4, "Ge": +4,"O":-2})
+        self.describer = DistinctSiteProperty(CNs=[6,8,4],
+                                              properties=["atomic_radius","X","ionic_radius"])
 
     def test_describe(self):
-        descriptor = self.describer.describe(self.li2o)
-        self.assertAlmostEqual(descriptor["8c-Z"], 3)
-        self.assertAlmostEqual(descriptor["8c-atomic_radius"], 1.45)
-        descriptor = self.describer.describe(self.na2o)
-        self.assertEqual(descriptor["4a-Z"], 8)
-        self.assertEqual(descriptor["4a-atomic_radius"], 0.6)
+        descriptor = self.describer.describe(self.y3al5o12,exclude_ele=['O'])
+        self.assertAlmostEqual(descriptor["6-atomic_radius"], 1.25)
+        self.assertAlmostEqual(descriptor["8-atomic_radius"], 1.8)
+        self.assertAlmostEqual(descriptor["4-atomic_radius"], 1.25)
+        self.assertAlmostEqual(descriptor["8-X"], 1.22)
+        self.assertAlmostEqual(descriptor["8-ionic_radius"], 1.04)
+        descriptor = self.describer.describe(self.basg,exclude_ele=['O'])
+        self.assertAlmostEqual(descriptor["4-ionic_radius"],0.62666666666666671)
+        self.assertAlmostEqual(descriptor["4-X"], 1.9724169529461031)
 
     def test_describe_all(self):
-        df = pd.DataFrame(self.describer.describe_all([self.li2o, self.na2o]))
-        self.assertEqual(df.iloc[0]["8c-Z"], 3)
-        self.assertEqual(df.iloc[0]["8c-atomic_radius"], 1.45)
+        df = pd.DataFrame(self.describer.describe_all([self.y3al5o12, self.basg]))
+        self.assertEqual(df.iloc[0]["6-atomic_radius"], 1.25)
+        self.assertEqual(df.iloc[1]["4-ionic_radius"], 0.62666666666666671)
+
+
+#class MultiDescriberTest(unittest.TestCase):
+
+#    def test_describe(self):
+#        li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+#                                                "Li2O.cif"))
+#        na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+#                                                "Na2O.cif"))
+#        d1 = DistinctSiteProperty(['8c', '4a'], ["Z", "atomic_radius"])
+#        d2 = FuncGenerator(func_dict={"exp": "np.exp"}, append=False)
+#        d = MultiDescriber([d1, d2])
+
+#        results = d.describe(li2o)
+#        self.assertAlmostEqual(results.loc["8c-Z"]["exp"], np.exp(3))
+#        self.assertAlmostEqual(results.loc["8c-atomic_radius"]["exp"],
+#                               np.exp(1.45))
+
+#        df = d.describe_all([li2o, na2o])
+#        print(df)
 
 
 class MultiDescriberTest(unittest.TestCase):
 
     def test_describe(self):
-        li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                "Li2O.cif"))
-        na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                "Na2O.cif"))
-        d1 = DistinctSiteProperty(['8c', '4a'], ["Z", "atomic_radius"])
+        y3al5o12 = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                         "Y3Al5O12.cif"))
+        basg = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                     "Ba3Al2Ge2SiO12.cif"))
+        d1 = DistinctSiteProperty(CNs=[6, 8, 4],
+                             properties=["atomic_radius", "X", "ionic_radius"])
         d2 = FuncGenerator(func_dict={"exp": "np.exp"}, append=False)
         d = MultiDescriber([d1, d2])
 
-        results = d.describe(li2o)
-        self.assertAlmostEqual(results.loc["8c-Z"]["exp"], np.exp(3))
-        self.assertAlmostEqual(results.loc["8c-atomic_radius"]["exp"],
-                               np.exp(1.45))
+        results = d.describe(y3al5o12)
+        self.assertAlmostEqual(results.loc["6-atomic_radius"]["exp"], np.exp(1.25))
+        self.assertAlmostEqual(results.loc["8-atomic_radius"]["exp"],
+                               np.exp(1.8))
 
-        df = d.describe_all([li2o, na2o])
+        df = d.describe_all([y3al5o12, basg])
         print(df)
+
 
 
 if __name__ == "__main__":
