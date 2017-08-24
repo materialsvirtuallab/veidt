3a4d1e1e836b9befeef420d867f0c7f7e359df7d
Zhi Deng <adengz@users.noreply.github.com>
2017-08-24 10:01:20 -0700

    Hackathon Aug24 (#11)
    * add feature cache functionality to lm
    
    * add results as attributes, and more independent tests for lm

--- veidt/tests/test_models.py
+++ veidt/tests/test_models.py
@@ -8,8 +8,10 @@ import os
 import json
 
 import numpy as np
+import pandas as pd
 from pymatgen import Structure
 
+from veidt.abstract import Describer
 from veidt.descriptors import DistinctSiteProperty
 from veidt.models import NeuralNet, LinearModel
 from monty.serialization import MontyEncoder, MontyDecoder
@@ -44,24 +46,40 @@ class NeuralNetTest(unittest.TestCase):
 
 class LinearModelTest(unittest.TestCase):
 
-    def setUp(self):
-        self.model = LinearModel(
-            describer=DistinctSiteProperty(['8c'], ["Z"]))
+    @classmethod
+    def setUpClass(cls):
+        cls.x_train = np.random.rand(10, 2)
+        cls.coef = np.random.rand(2)
+        cls.intercept = np.random.rand()
+        cls.y_train = cls.x_train.dot(cls.coef) + cls.intercept
 
-    def test_fit_evaluate(self):
-        li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                "Li2O.cif"))
-        na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                "Na2O.cif"))
-        structures = [li2o] * 100 + [na2o] * 100
-        energies = [3] * 100 + [4] * 100
-        energies += np.random.randn(200)
-        self.model.fit(inputs=structures, outputs=energies)
-        # Given this is a fairly simple model, we should get close to exact.
-        self.assertEqual(round(self.model.predict([na2o])[0]), 4, 3)
+    def setUp(self):
+        class DummyDescriber(Describer):
+
+            def describe(self, obj):
+                pass
+
+            def describe_all(self, n):
+                return pd.DataFrame(n)
+
+        self.lm = LinearModel(DummyDescriber())
+
+    def test_fit_predict(self):
+        self.lm.fit(inputs=self.x_train, outputs=self.y_train)
+        x_test = np.random.rand(10, 2)
+        y_test = x_test.dot(self.coef) + self.intercept
+        y_pred = self.lm.predict(x_test)
+        np.testing.assert_array_almost_equal(y_test, y_pred)
+        np.testing.assert_array_almost_equal(self.coef, self.lm.coef)
+        self.assertAlmostEqual(self.intercept, self.lm.intercept)
+
+    def test_evaluate_fit(self):
+        self.lm.fit(inputs=self.x_train, outputs=self.y_train)
+        y_pred = self.lm.evaluate_fit()
+        np.testing.assert_array_almost_equal(y_pred, self.y_train)
 
     def test_serialize(self):
-        json_str = json.dumps(self.model.as_dict())
+        json_str = json.dumps(self.lm.as_dict())
         recover = LinearModel.from_dict(json.loads(json_str))
         self.assertIsNotNone(recover)
 
