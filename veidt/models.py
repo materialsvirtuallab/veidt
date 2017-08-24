2ff2cad9cef3f261a62284b5d864e86dcf9a6e3d
w6ye <w6ye@ucsd.edu>
2017-08-24 12:47:04 -0700

    updated model save and load

--- veidt/models.py
+++ veidt/models.py
@@ -3,13 +3,12 @@
 # Distributed under the terms of the BSD License.
 
 import sys
-import warnings
 
 from veidt.abstract import Model
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.externals import joblib
 
 class NeuralNet(Model):
     """
@@ -32,23 +31,22 @@ class NeuralNet(Model):
         self.loss = loss
         self.model = None
 
-    def fit(self, inputs, outputs, test_size=0.2, **kwargs):
+    def fit(self, inputs, outputs, test_size=0.2, adam_lr = 1e-2, **kwargs):
         """
         :param inputs: List of inputs
         :param outputs: List of outputs
         :param test_size: Size of test set. Defaults to 0.2.
+        :param adam_lr: learning rate of Adam optimizer
         :param kwargs: Passthrough to fit function in keras.models
         """
         from keras.optimizers import Adam
         from keras.models import Sequential
         from keras.layers import Dense
         descriptors = self.describer.describe_all(inputs)
-        if self.preprocessor is None:
+        if not self.preprocessor:
             self.preprocessor = StandardScaler()
-            scaled_descriptors = self.preprocessor.fit_transform(descriptors)
-        else:
-            scaled_descriptors = self.preprocessor.transform(descriptors)
-        adam = Adam(1e-2)
+        scaled_descriptors = self.preprocessor.fit_transform(descriptors)
+        adam = Adam(adam_lr)
         x_train, x_test, y_train, y_test = train_test_split(
             scaled_descriptors, outputs, test_size=test_size)
 
@@ -68,17 +66,34 @@ class NeuralNet(Model):
         scaled_descriptors = self.preprocessor.transform(descriptors)
         return self.model.predict(scaled_descriptors)
 
-    def save(self, model_fname):
+    def model_save(self, model_fname, scaler_fname):
+        """
+        use kears model.save method to save model in .h5
+        use scklearn.external.joblib to save scaler(the .save
+        file is supposed to be much smaller than saved as
+        pickle file)
+        :param model_fname:
+        :param scaler_fname:
+        :return:None
+        """
         self.model.save(model_fname)
+        joblib.dump(self.preprocessor, scaler_fname)
+
+    def model_load(self, model_fname, scaler_fname):
+        from keras.models import load_model
+        self.model = load_model(model_fname)
+        self.preprocessor = joblib.load(scaler_fname)
+
+
 
 
 class LinearModel(Model):
     """
     Linear model.
 
-    :param describer: Describer To convert input objects to
+    :param describer: Desciber object to convert input objects to
         descriptors.
-    :param regressor: (str) Name of LinearModel from
+    :param regressor (str): Name of LinearModel from
         sklearn.linear_model. Default to "LinearRegression", i.e.,
         ordinary least squares.
     :param kwargs: kwargs to be passed to regressor.
@@ -92,70 +107,21 @@ class LinearModel(Model):
         lm = sys.modules["sklearn.linear_model"]
         lr = getattr(lm, regressor)
         self.model = lr(**kwargs)
-        self._xtrain = None
-        self._xtest = None
 
-    def fit(self, inputs, outputs, weights=None, override=False):
+    def fit(self, inputs, outputs):
         """
-        Fit model.
-
-        :param inputs: List of input training objects.
-        :param outputs: List/Array of output values (supervisory
-            signals).
-        :param weights: List/Array of weights. Default to None, i.e.,
-            unweighted.
-        :param override: (bool) Whether to calculate the feature
-            vectors from given inputs. Default to False. Set to True if
-            you want to retrain the model with a different set of
-            training inputs.
+        :param inputs: List of inputs
+        :param outputs: List of outputs
         """
-        if self._xtrain is None or override:
-            xtrain = self.describer.describe_all(inputs)
-        else:
-            warnings.warn("Feature vectors retrieved from cache "
-                          "and input training objects ignored. "
-                          "To override the old cache with feature vectors "
-                          "of new training objects, set override=True.")
-            xtrain = self._xtrain
-        self.model.fit(xtrain, outputs, weights)
-        self._xtrain = xtrain
+        descriptors = self.describer.describe_all(inputs)
+        self.model.fit(descriptors, outputs)
 
-    def predict(self, inputs, override=False):
-        """
-        Predict outputs with fitted model.
+    def predict(self, inputs):
+        descriptors = self.describer.describe_all(inputs)
+        return self.model.predict(descriptors)
 
-        :param inputs: List of input testing objects.
-        :param override: (bool) Whether to calculate the feature
-            vectors from given inputs. Default to False. Set to True if
-            you want to test the model with a different set of testing
-            inputs.
-        :return: Predicted output array from inputs.
-        """
-        if self._xtest is None or override:
-            xtest = self.describer.describe_all(inputs)
-        else:
-            warnings.warn("Feature vectors retrieved from cache "
-                          "and input testing objects ignored. "
-                          "To override the old cache with feature vectors "
-                          "of new testing objects, set override=True.")
-            xtest = self._xtest
-        self._xtest = xtest
-        return self.model.predict(xtest)
+    def model_save(self, model_fname):
+        joblib.dump(self.model, '%s.pkl' % model_fname)
 
-    def evaluate_fit(self):
-        """
-        Efficient method to obtain prediction on training inputs w/o
-        calculating the features of inputs again.
-
-        :return: Predicted output array from training inputs.
-        """
-        self._xtest = self._xtrain
-        return self.predict(inputs=None, override=False)
-
-    @property
-    def coef(self):
-        return self.model.coef_
-
-    @property
-    def intercept(self):
-        return self.model.intercept_
+    def model_load(self, model_fname):
+        self.model = joblib.load(model_fname)
--- veidt/tests/test_models.py
+++ veidt/tests/test_models.py
@@ -2,26 +2,60 @@
 
 from __future__ import division, print_function, unicode_literals, \
     absolute_import
-
+import shutil, tempfile
 import unittest
 import os
 import json
 
 import numpy as np
-import pandas as pd
 from pymatgen import Structure
 
-from veidt.abstract import Describer
 from veidt.descriptors import DistinctSiteProperty
 from veidt.models import NeuralNet, LinearModel
-from monty.serialization import MontyEncoder, MontyDecoder
 
 
 class NeuralNetTest(unittest.TestCase):
-
     def setUp(self):
-        self.model = NeuralNet(
-            [25, 5], describer=DistinctSiteProperty(['8c'], ["Z"]))
+        self.nn = NeuralNet(
+            [20], describer=DistinctSiteProperty(['8c'], ["Z"]))
+        self.nn2 = NeuralNet(
+            [20], describer=DistinctSiteProperty(['8c'], ["Z"]))
+        self.li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                "Li2O.cif"))
+        self.na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                "Na2O.cif"))
+        self.structures = [self.li2o] * 100 + [self.na2o] * 100
+        self.energies = [3] * 100 + [4] * 100
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.test_dir)
+
+    def test_fit_evaluate(self):
+
+        self.nn.fit(inputs=self.structures, outputs=self.energies, epochs=100)
+        # Given this is a fairly simple model, we should get close to exact.
+        self.assertEqual(round(self.nn.predict([self.na2o])[0][0]), 4, 3)
+
+    def test_model_save_load(self):
+        model_fname = os.path.join(self.test_dir, 'test_nnmodel.h5')
+        scaler_fname = os.path.join(self.test_dir, 'test_nnscaler.save')
+        self.nn.fit(inputs=self.structures, outputs=self.energies, epochs=100)
+        self.nn.model_save(model_fname=model_fname, scaler_fname=scaler_fname)
+        self.nn2.model_load(model_fname=model_fname, scaler_fname=scaler_fname)
+        self.assertEqual(self.nn.predict([self.na2o])[0][0], self.nn2.predict([self.na2o])[0][0])
+
+
+class LinearModelTest(unittest.TestCase):
+    def setUp(self):
+        self.lm = LinearModel(
+            describer=DistinctSiteProperty(['8c'], ["Z"]))
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.test_dir)
 
     def test_fit_evaluate(self):
         li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
@@ -30,58 +64,24 @@ class NeuralNetTest(unittest.TestCase):
                                                 "Na2O.cif"))
         structures = [li2o] * 100 + [na2o] * 100
         energies = [3] * 100 + [4] * 100
-        self.model.fit(inputs=structures, outputs=energies, nb_epoch=100)
+        energies += np.random.randn(200)
+        self.lm.fit(inputs=structures, outputs=energies)
         # Given this is a fairly simple model, we should get close to exact.
-        self.assertEqual(round(self.model.predict([na2o])[0][0]), 4, 3)
-
-        self.model.save("nntest.h5")
-        self.assertTrue(os.path.exists("nntest.h5"))
-        os.remove("nntest.h5")
-
-    # def test_serialize(self):
-    #     json_str = json.dumps(self.model, cls=MontyEncoder)
-    #     recover = LinearModel.from_dict(json.loads(json_str, cls=MontyDecoder))
-    #     self.assertIsNotNone(recover)
-
-
-class LinearModelTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.x_train = np.random.rand(10, 2)
-        cls.coef = np.random.rand(2)
-        cls.intercept = np.random.rand()
-        cls.y_train = cls.x_train.dot(cls.coef) + cls.intercept
-
-    def setUp(self):
-        class DummyDescriber(Describer):
-
-            def describe(self, obj):
-                pass
-
-            def describe_all(self, n):
-                return pd.DataFrame(n)
-
-        self.lm = LinearModel(DummyDescriber())
-
-    def test_fit_predict(self):
-        self.lm.fit(inputs=self.x_train, outputs=self.y_train)
-        x_test = np.random.rand(10, 2)
-        y_test = x_test.dot(self.coef) + self.intercept
-        y_pred = self.lm.predict(x_test)
-        np.testing.assert_array_almost_equal(y_test, y_pred)
-        np.testing.assert_array_almost_equal(self.coef, self.lm.coef)
-        self.assertAlmostEqual(self.intercept, self.lm.intercept)
-
-    def test_evaluate_fit(self):
-        self.lm.fit(inputs=self.x_train, outputs=self.y_train)
-        y_pred = self.lm.evaluate_fit()
-        np.testing.assert_array_almost_equal(y_pred, self.y_train)
+        self.assertEqual(round(self.lm.predict([na2o])[0]), 4, 3)
 
     def test_serialize(self):
         json_str = json.dumps(self.lm.as_dict())
         recover = LinearModel.from_dict(json.loads(json_str))
-        self.assertIsNotNone(recover)
+        self.assert_(True)
+
+    def model_save_load(self):
+        self.lm.model_save(os.path.join(self.test_dir, 'test_lm.save'))
+        ori = self.lm.model.coef_
+        load_m = self.lm.model_load(os.path.join(self.test_dir, 'test_lm.save'))
+        loaded = self.lm.model.coef_
+        self.assertAlmostEqual(ori, loaded)
+
+
 
 if __name__ == "__main__":
     unittest.main()
