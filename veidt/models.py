29b995329e5b2d320b80f125c8f21bf6d74f67e1
w6ye <w6ye@ucsd.edu>
2017-08-24 20:36:21 -0700

    Add model and scaler save and load methods to models (#14)
    * updated model save and load
    
    * fix conflicts
    
    * small format fix
    
    * method name change

--- veidt/models.py
+++ veidt/models.py
@@ -9,6 +9,7 @@ from veidt.abstract import Model
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 
 
 class NeuralNet(Model):
@@ -32,11 +33,12 @@ class NeuralNet(Model):
         self.loss = loss
         self.model = None
 
-    def fit(self, inputs, outputs, test_size=0.2, **kwargs):
+    def fit(self, inputs, outputs, test_size=0.2, adam_lr=1e-2, **kwargs):
         """
         :param inputs: List of inputs
         :param outputs: List of outputs
         :param test_size: Size of test set. Defaults to 0.2.
+        :param adam_lr: learning rate of Adam optimizer
         :param kwargs: Passthrough to fit function in keras.models
         """
         from keras.optimizers import Adam
@@ -48,7 +50,7 @@ class NeuralNet(Model):
             scaled_descriptors = self.preprocessor.fit_transform(descriptors)
         else:
             scaled_descriptors = self.preprocessor.transform(descriptors)
-        adam = Adam(1e-2)
+        adam = Adam(adam_lr)
         x_train, x_test, y_train, y_test = train_test_split(
             scaled_descriptors, outputs, test_size=test_size)
 
@@ -68,8 +70,23 @@ class NeuralNet(Model):
         scaled_descriptors = self.preprocessor.transform(descriptors)
         return self.model.predict(scaled_descriptors)
 
-    def save(self, model_fname):
+    def save_model(self, model_fname, scaler_fname):
+        """
+        use kears model.save method to save model in .h5
+        use scklearn.external.joblib to save scaler(the .save
+        file is supposed to be much smaller than saved as
+        pickle file)
+        :param model_fname:
+        :param scaler_fname:
+        :return:None
+        """
         self.model.save(model_fname)
+        joblib.dump(self.preprocessor, scaler_fname)
+
+    def load_model(self, model_fname, scaler_fname):
+        from keras.models import load_model
+        self.model = load_model(model_fname)
+        self.preprocessor = joblib.load(scaler_fname)
 
 
 class LinearModel(Model):
@@ -159,3 +176,9 @@ class LinearModel(Model):
     @property
     def intercept(self):
         return self.model.intercept_
+
+    def save_model(self, model_fname):
+        joblib.dump(self.model, '%s.pkl' % model_fname)
+
+    def load_model(self, model_fname):
+        self.model = joblib.load(model_fname)
--- veidt/tests/test_models.py
+++ veidt/tests/test_models.py
@@ -15,37 +15,42 @@ from veidt.abstract import Describer
 from veidt.descriptors import DistinctSiteProperty
 from veidt.models import NeuralNet, LinearModel
 from monty.serialization import MontyEncoder, MontyDecoder
+import shutil, tempfile
 
 
 class NeuralNetTest(unittest.TestCase):
-
     def setUp(self):
-        self.model = NeuralNet(
+        self.nn = NeuralNet(
             [25, 5], describer=DistinctSiteProperty(['8c'], ["Z"]))
+        self.nn2 = NeuralNet(
+            [25, 5], describer=DistinctSiteProperty(['8c'], ["Z"]))
+        self.li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                     "Li2O.cif"))
+        self.na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
+                                                     "Na2O.cif"))
+        self.structures = [self.li2o] * 100 + [self.na2o] * 100
+        self.energies = [3] * 100 + [4] * 100
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.test_dir)
 
     def test_fit_evaluate(self):
-        li2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                "Li2O.cif"))
-        na2o = Structure.from_file(os.path.join(os.path.dirname(__file__),
-                                                "Na2O.cif"))
-        structures = [li2o] * 100 + [na2o] * 100
-        energies = [3] * 100 + [4] * 100
-        self.model.fit(inputs=structures, outputs=energies, nb_epoch=100)
+        self.nn.fit(inputs=self.structures, outputs=self.energies, nb_epoch=100)
         # Given this is a fairly simple model, we should get close to exact.
-        self.assertEqual(round(self.model.predict([na2o])[0][0]), 4, 3)
+        self.assertEqual(round(self.nn.predict([self.na2o])[0][0]), 4, 3)
 
-        self.model.save("nntest.h5")
-        self.assertTrue(os.path.exists("nntest.h5"))
-        os.remove("nntest.h5")
-
-    # def test_serialize(self):
-    #     json_str = json.dumps(self.model, cls=MontyEncoder)
-    #     recover = LinearModel.from_dict(json.loads(json_str, cls=MontyDecoder))
-    #     self.assertIsNotNone(recover)
+    def test_model_save_load(self):
+        model_fname = os.path.join(self.test_dir, 'test_nnmodel.h5')
+        scaler_fname = os.path.join(self.test_dir, 'test_nnscaler.save')
+        self.nn.fit(inputs=self.structures, outputs=self.energies, epochs=100)
+        self.nn.model_save(model_fname=model_fname, scaler_fname=scaler_fname)
+        self.nn2.model_load(model_fname=model_fname, scaler_fname=scaler_fname)
+        self.assertEqual(self.nn.predict([self.na2o])[0][0], self.nn2.predict([self.na2o])[0][0])
 
 
 class LinearModelTest(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.x_train = np.random.rand(10, 2)
@@ -55,7 +60,6 @@ class LinearModelTest(unittest.TestCase):
 
     def setUp(self):
         class DummyDescriber(Describer):
-
             def describe(self, obj):
                 pass
 
@@ -63,6 +67,12 @@ class LinearModelTest(unittest.TestCase):
                 return pd.DataFrame(n)
 
         self.lm = LinearModel(DummyDescriber())
+
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.test_dir)
 
     def test_fit_predict(self):
         self.lm.fit(inputs=self.x_train, outputs=self.y_train)
@@ -82,6 +92,14 @@ class LinearModelTest(unittest.TestCase):
         json_str = json.dumps(self.lm.as_dict())
         recover = LinearModel.from_dict(json.loads(json_str))
         self.assertIsNotNone(recover)
+
+    def model_save_load(self):
+        self.lm.model_save(os.path.join(self.test_dir, 'test_lm.save'))
+        ori = self.lm.model.coef_
+        self.lm.model_load(os.path.join(self.test_dir, 'test_lm.save'))
+        loaded = self.lm.model.coef_
+        self.assertAlmostEqual(ori, loaded)
+
 
 if __name__ == "__main__":
     unittest.main()
