d76957154f6644eefc281403ca84bf92ec9bc6a2
czhengsci <czhengsci@gmail.com>
2017-09-16 15:47:22 -0700

    add more test

--- veidt/ELSIE/tests/test_ensemble.py
+++ veidt/ELSIE/tests/test_ensemble.py
@@ -13,7 +13,7 @@ ensemble_list = [[value['Preprocessing'], value['Similarity_metric']] for _, val
 
 class EnsembleRankTest(unittest.TestCase):
     def setUp(self):
-        self.x_range = np.linspace(-np.pi, np.pi, 201)
+        self.x_range = np.linspace(-np.pi*5, np.pi*5, 2001)
         self.target_spect = np.column_stack((self.x_range, abs(np.sin(self.x_range))))
         self.spect_to_identify = pd.DataFrame([(x, deepcopy(self.target_spect)) for x in ['A', 'B', 'C', 'D']],
                                               columns=['unique_key', 'ref_spect'])
@@ -22,3 +22,16 @@ class EnsembleRankTest(unittest.TestCase):
     def test_borda_rank_vote(self):
         self.ensemble_test.borda_rank_vote(ensemble_list)
         self.assertEqual(np.unique(self.ensemble_test.dataframe['energy_shift']), 0.0)
+        self.ensemble_test.calculate_softmax_prob()
+
+
+class SimpleEnsembleTest(unittest.TestCase):
+    def setUp(self):
+        self.x_range = np.linspace(-np.pi*5, np.pi*5, 2001)
+        self.target_spect = np.column_stack((self.x_range, abs(np.sin(self.x_range))))
+        self.ref_spect = [deepcopy(self.target_spect)] * 3
+        self.simple_test = SimpleEnsemble(self.target_spect, self.ref_spect)
+
+    def test_preprocess_similarity_compute(self):
+        self.simple_test.preprocess_similarity_compute('intnorm', 'Cosine')
+        self.assertTrue(np.allclose(self.simple_test.spect_df['Similarity'], 1.0))
