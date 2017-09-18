bb8b244a32cb44ec12612a666970e0825adef0cc
__sp__ <ongsp@eng.ucsd.edu>
2017-09-18 13:15:16 -0700

    Update tasks.

--- tasks.py
+++ tasks.py
@@ -1,9 +1,11 @@
 from invoke import task
 import os
 import webbrowser
-import datetime
-
+import re
+import json
+import requests
 from monty.os import cd
+from veidt import __version__ as VERSION
 
 """
 Deployment file to facilitate releases of veidt.
@@ -47,3 +49,43 @@ def update_doc(ctx):
 def open_doc(ctx):
     pth = os.path.abspath("docs/index.html")
     webbrowser.open("file://" + pth)
+
+
+@task
+def publish(ctx):
+    ctx.run("rm dist/*.*", warn=True)
+    ctx.run("python setup.py register sdist bdist_wheel")
+    ctx.run("twine upload dist/*")
+
+@task
+def release_github(ctx):
+    with open("CHANGES.rst") as f:
+        contents = f.read()
+    toks = re.split("\-+", contents)
+    desc = toks[1].strip()
+    toks = desc.split("\n")
+    desc = "\n".join(toks[:-1]).strip()
+    payload = {
+        "tag_name": "v" + VERSION,
+        "target_commitish": "master",
+        "name": "v" + VERSION,
+        "body": desc,
+        "draft": False,
+        "prerelease": False
+    }
+    response = requests.post(
+        "https://api.github.com/repos/materialsproject/pymatgen/releases",
+        data=json.dumps(payload),
+        headers={"Authorization": "token " + os.environ["GITHUB_RELEASES_TOKEN"]})
+    print(response.text)
+
+
+@task
+def release(ctx, notest=False):
+    if not notest:
+        ctx.run("nosetests")
+    publish(ctx)
+    update_doc(ctx)
+    ctx.run("git tag -a v%s -m \"v%s release\"" % (VERSION, VERSION))
+    ctx.run("git push --tags")
+    release_github(ctx)
