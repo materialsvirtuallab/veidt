a98337e1ab655d74d47da00bc9196b7252ce4983
Chi Chen <chc273@eng.ucsd.edu>
2018-07-13 14:00:16 -0700

    add new potential abstract class

--- veidt/potential/abstract.py
+++ veidt/potential/abstract.py
@@ -3,10 +3,11 @@
 # Distributed under the terms of the BSD License.
 
 import abc
+from veidt.abstract import Model
+import abc
 import six
 from monty.json import MSONable
-from veidt.metrics import get
-from collections import defaultdict
+
 
 class Potential(six.with_metaclass(abc.ABCMeta, MSONable)):
     """
@@ -14,40 +15,10 @@ class Potential(six.with_metaclass(abc.ABCMeta, MSONable)):
     """
 
     @abc.abstractmethod
-    def fit(self, features, energies, forces, stresses, **kwargs):
-        """
-        Train interatomic potential with energies, forces and
-        stresses corresponding to structures.
-
-        :param features: List of numerical features for energy, force and stress
-        :param energies: List of DFT-calculated total energies of each structure
-            in structures list.
-        :param forces: List of DFT-calculated (m, 3) forces of each structure
-            with m atoms in structures list. m can be varied with each single
-            structure case.
-        :param stresses: List of DFT-calculated (6, ) virial stresses of each
-            structure in structures list.
-        """
-        pass
-
-    @abc.abstractmethod
-    def predict_from_features(self, features):
-        """
-        predict energy, force and stress from numerical features
-        :param features: List of numerical features
-        :return: (energies, forces, stresses)
-        """
-        pass
-
-    def predict(self, structures):
-        features = self.describer.transform(structures)
-        return self.predict_from_features(features)
-
     def train(self, train_structures, energies, forces, stresses, **kwargs):
         """
         Train interatomic potential with energies, forces and
         stresses corresponding to structures.
-
         :param train_structures: List of Pymatgen Structure objects.
         :param energies: List of DFT-calculated total energies of each structure
             in structures list.
@@ -57,11 +28,47 @@ class Potential(six.with_metaclass(abc.ABCMeta, MSONable)):
         :param stresses: List of DFT-calculated (6, ) virial stresses of each
             structure in structures list.
         """
+        pass
+
+    @abc.abstractmethod
+    def evaluate(self, test_structures, ref_energies, ref_forces, ref_stresses):
+        """
+        Evaluate energies, forces and stresses of structures with trained
+        interatomic potential.
+        :param test_structures: List of Pymatgen Structure Objects.
+        :param ref_energies: List of DFT-calculated total energies of each
+            structure in structures list.
+        :param ref_forces: List of DFT-calculated (m, 3) forces of each
+            structure with m atoms in structures list. m can be varied with
+            each single structure case.
+        :param ref_stresses: List of DFT-calculated (6, ) viriral stresses of
+            each structure in structures list.
+        :return: DataFrame of original data and DataFrame of predicted data.
+        """
+        pass
+
+
+class PotentialVeidt(Model):
+    """
+    Abstract Base class for a interatomic Potential.
+    """
+
+    def predict_structures(self, structures):
+        features = self.describer.transform(structures)
+        return self.predict(features)
+
+    def train(self, train_structures, targets, **kwargs):
+        """
+        Train interatomic potential with energies, forces and
+        stresses corresponding to structures.
+
+        :param train_structures: List of Pymatgen Structure objects.
+        :param targets: targets that can be directly trained
+        """
         features = self.describer.transform(train_structures)
-        self.fit(features, energies, forces, stresses, **kwargs)
+        self.fit(features, targets, **kwargs)
 
-
-    def evaluate(self, test_structures, ref_energies, ref_forces, ref_stresses, metrics=['mae']):
+    def evaluate_structures(self, test_structures, targets, metrics=['mae']):
         """
         Evaluate energies, forces and stresses of structures with trained
         interatomic potential.
@@ -77,11 +84,6 @@ class Potential(six.with_metaclass(abc.ABCMeta, MSONable)):
 
         :return: DataFrame of original data and DataFrame of predicted data.
         """
-        pred_energies, pred_forces, pred_stresses = self.predict(test_structures)
-        evaluation = defaultdict(dict)
-        for metric in metrics:
-            veidt_metric = get(metric)
-            evaluation['metric']['energy'] = veidt_metric(ref_energies, pred_energies)
-            evaluation['metric']['force'] = veidt_metric(ref_forces, pred_forces)
-            evaluation['metric']['stress'] = veidt_metric(ref_stresses, pred_stresses)
+        features = self.describer.transform(test_structures)
+        evaluation = self.evaluate(features, targets, metrics=metrics, multi_targets=False)
         return evaluation
